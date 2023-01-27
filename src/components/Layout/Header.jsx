795f81b8e011f7cb3336c03b92196701f6206b31
Beeni <beeni4429@gmail.com>
2023-01-28 00:34:58 +0500

    fix header and homepage components

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,43 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { API_URL, ASSET_URL } from '../../utils';
+import { API_URL, ASSET_URL } from "../../utils";
 import { Link as ScrollLink } from "react-scroll";
 import { Link, useRouteMatch } from "react-router-dom";
 import axios from "axios";
 import {
   FaFacebookF,
-  FaTwitter,
-  FaInstagram,
-  FaYoutube,
+  FaTwittgram,
+  FaYoutuer,
+  FaInstabe,
   FaDribbble,
+  FaGithub,
+  FaLinkedin,
+  FaBehance,
+  FaGitlab,
 } from "react-icons/fa";
-
-const headerData = {
-  social: {
-    facebook: "https://facebook.com",
-    twitter: "https://twitter.com",
-    instagram: "https://www.instagram.com/",
-    youtue: "https://www.youtube.com/",
-    dribbble: "https://dribbble.com/",
-  },
-};
 
 function Header({ toggleHeader, toggleHandler }) {
   const [currentPath, setCurrentPath] = useState("");
   const match = useRouteMatch();
   const [userData, setUserData] = useState();
+  const [socialNetwork, setSocialNetwork] = useState();
 
   useEffect(() => {
     setCurrentPath(match.path);
   }, [match]);
 
-
   useEffect(() => {
-    axios.get(`${API_URL}/about-mes?populate=*`)
-      .then(res => {
-        setUserData(res.data?.data[0]?.attributes)
-      })
+    axios.get(`${API_URL}/about-mes?populate=*`).then((res) => {
+      setUserData(res.data?.data[0]?.attributes);
+    });
   }, []);
 
+  useEffect(() => {
+    axios.get(`${API_URL}/social-networks`).then((res) => {
+      setSocialNetwork(res.data?.data);
+    });
+  }, []);
 
   return (
     <>
@@ -54,7 +52,10 @@ function Header({ toggleHeader, toggleHandler }) {
           <span></span>
         </button>
         <Link to="/" className="logo">
-          <img src={`${ASSET_URL}${userData?.profilePhoto?.data?.attributes?.url}`} alt={userData?.name} />
+          <img
+            src={`${ASSET_URL}${userData?.profilePhoto?.data?.attributes?.url}`}
+            alt={userData?.name}
+          />
         </Link>
         <Link to="/" className="site-title dot ml-2">
           {userData?.name}
@@ -77,8 +78,16 @@ function Header({ toggleHeader, toggleHandler }) {
           <span aria-hidden="true">&times;</span>
         </button>
         <div className="header-inner d-flex align-items-start flex-column">
-          <Link to="/" >
-            <img src={`${ASSET_URL}${userData?.profilePhoto?.data?.attributes?.url}`} alt={userData?.name} style={{ maxHeight: "5.6rem", maxWidth: "5.6rem", borderRadius: "50%" }} />
+          <Link to="/">
+            <img
+              src={`${ASSET_URL}${userData?.profilePhoto?.data?.attributes?.url}`}
+              alt={userData?.name}
+              style={{
+                maxHeight: "5.6rem",
+                maxWidth: "5.6rem",
+                borderRadius: "50%",
+              }}
+            />
           </Link>
           <Link to="/" className="site-title dot mt-3">
             {userData?.name}
@@ -128,17 +137,17 @@ function Header({ toggleHeader, toggleHandler }) {
                 {currentPath === "/" ? (
                   <ScrollLink
                     activeClass="active"
-                    to="section-services"
+                    to="section-skills"
                     spy={true}
                     smooth={true}
                     offset={-50}
                     duration={500}
                   >
-                    <i className="icon-bulb"></i>Services
+                    <i className="icon-bulb"></i>Skills
                   </ScrollLink>
                 ) : (
                   <Link to="/">
-                    <i className="icon-bulb"></i>Services
+                    <i className="icon-bulb"></i>Skills
                   </Link>
                 )}
               </li>
@@ -146,7 +155,7 @@ function Header({ toggleHeader, toggleHandler }) {
                 {currentPath === "/" ? (
                   <ScrollLink
                     activeClass="active"
-                    to="section-experiences"
+                    to="section-Resume"
                     spy={true}
                     smooth={true}
                     offset={-50}
@@ -198,49 +207,21 @@ function Header({ toggleHeader, toggleHandler }) {
               </li>
             </ul>
           </nav>
-
           <div className="footer ">
             <ul className="social-icons list-inline">
-              {!headerData.social.facebook ? null : (
+              {socialNetwork?.map((item) => (
                 <li className="list-inline-item">
-                  <a href={headerData.social.facebook} target="_blank" without rel="noreferrer">
-                    <FaFacebookF />
+                  <a
+                    href={item?.attributes?.link}
+                    target="_blank"
+                    without
+                    rel="noreferrer"
+                  >
+                    {<FaBehance />}
                   </a>
                 </li>
-              )}
-              {!headerData.social.twitter ? null : (
-                <li className="list-inline-item">
-                  <a href={headerData.social.twitter} target={"_blank"} rel="noreferrer">
-                    <FaTwitter />
-                  </a>
-                </li>
-              )}
-              {!headerData.social.instagram ? null : (
-                <li className="list-inline-item">
-                  <a href={headerData.social.instagram} target={"_blank"} rel="noreferrer">
-                    <FaInstagram />
-                  </a>
-                </li>
-              )}
-              {!headerData.social.youtue ? null : (
-                <li className="list-inline-item">
-                  <a href={headerData.social.youtue} target={"_blank"} rel="noreferrer">
-                    <FaYoutube />
-                  </a>
-                </li>
-              )}
-              {!headerData.social.dribbble ? null : (
-                <li className="list-inline-item">
-                  <a href={headerData.social.dribbble} target={"_blank"} rel="noreferrer">
-                    <FaDribbble />
-                  </a>
-                </li>
-              )}
+              ))}
             </ul>
-
-            <span className="copyright">
-              &copy; {new Date().getFullYear()} Bako Template
-            </span>
           </div>
         </div>
       </header>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,24 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Element } from "react-scroll";
+import { Helmet } from "react-helmet";
 import Layout from "../components/Layout/Layout";
 import About from "../components/Sections/About";
-import Testimonials from "../components/Sections/Testimonials";
 import Herosection from "../components/Sections/Herosection";
 import SectionHeading from "../components/Items/SectionHeading";
 import Skills from "../components/Sections/Skills";
-import Funfacts from "../components/Sections/Funfacts";
 import Services from "../components/Sections/Services";
 import Experiences from "../components/Sections/Experiences";
 import Portfolios from "../components/Sections/Portfolios";
-import PricingTable from "../components/Sections/PricingTable";
 import Contact from "../components/Sections/Contact";
+import { API_URL } from "../utils";
 
 function Homepage() {
+  const [userData, setUserData] = useState();
+  useEffect(() => {
+    axios.get(`${API_URL}/about-mes?populate=*`).then((res) => {
+      setUserData(res.data?.data[0]?.attributes);
+    });
+  }, []);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
     <Layout>
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{userData?.name}</title>
+        </Helmet>
+      </div>
       <Element name="section-home">
         <Herosection />
       </Element>
@@ -37,21 +50,9 @@ function Homepage() {
         </section>
       </Element>
 
-      <Element name="section-funfacts">
-        <Funfacts />
-      </Element>
-
-      <Element name="section-services">
+      <Element name="section-Resume">
         <section className="shadow-blue white-bg padding">
-          <SectionHeading title="Services" />
-          <Services />
-        </section>
-      </Element>
-
-
-      <Element name="section-experiences">
-        <section className="shadow-blue white-bg padding">
-          <SectionHeading title="Experience" />
+          <SectionHeading title="Resume" />
           <Experiences />
         </section>
       </Element>
@@ -60,20 +61,6 @@ function Homepage() {
         <section className="shadow-blue white-bg padding">
           <SectionHeading title="Portfolio" />
           <Portfolios />
-        </section>
-      </Element>
-
-      <Element name="section-pricing">
-        <section className="shadow-blue white-bg padding">
-          <SectionHeading title="Pricing table" />
-          <PricingTable />
-        </section>
-      </Element>
-
-      <Element name="section-testimonials">
-        <section className="shadow-blue white-bg padding">
-          <SectionHeading title="Testimonials" />
-          <Testimonials />
         </section>
       </Element>
 
